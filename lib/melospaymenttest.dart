91c91ae489b7291d1ad2fc77bc6317ecba14d589
RashkaDahir <98263086+RashkaDahir@users.noreply.github.com>
2024-06-30 17:09:35 +0300

    first commit

---
+++ lib/melospaymenttest.dart
@@ -0,0 +1,25 @@
+library melospaymenttest;
+
+import 'package:flutter/material.dart';
+
+/// A Calculator.
+class MelostTest extends StatefulWidget {
+  const MelostTest({super.key});
+
+  @override
+  State<MelostTest> createState() => _MelostTestState();
+}
+
+class _MelostTestState extends State<MelostTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: Center(
+        child: Text('Rashka dahir'),
+      ),
+
+
+    );
+  }
+}
